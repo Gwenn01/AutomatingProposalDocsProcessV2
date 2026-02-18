aa557bc52239f95fe7be564d5904fec88750ed30
meuorii <fontillaskian@gmail.com>
2026-02-18 08:27:19 +0800

    Server Fix

--- server/reviewer/urls.py
+++ server/reviewer/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import ProposalReviewerViewSet
+from .views import AssignReviewerView
 
 urlpatterns = [
-    path('assign-reviewer/', ProposalReviewerViewSet.as_view(), name='assign-reviewer'),
+    path('assign-reviewer/', AssignReviewerView.as_view(), name='assign-reviewer'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
