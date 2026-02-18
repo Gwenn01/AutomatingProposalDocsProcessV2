9c4718784710e181ce6961bb17699c22f44849c7
Gwenn01 <arnelgwenn@gmail.com>
2026-02-18 10:54:06 +0800

    add child id

--- server/proposals_node/serializers.py
+++ server/proposals_node/serializers.py
@@ -2,6 +2,23 @@ from rest_framework import serializers
 from .models import Proposal
 
 class ProposalSerializer(serializers.ModelSerializer):
+    
+    child_id = serializers.SerializerMethodField()
+    
     class Meta:
         model = Proposal
         fields = '__all__'
+        extra_fields = ['child_id']
+        
+    def get_child_id(self, obj):
+
+        if obj.proposal_type == "Program" and hasattr(obj, 'program_details'):
+            return obj.program_details.id
+
+        if obj.proposal_type == "Project" and hasattr(obj, 'project_details'):
+            return obj.project_details.id
+
+        if obj.proposal_type == "Activity" and hasattr(obj, 'activity_details'):
+            return obj.activity_details.id
+
+        return None
