0c6bec45702034a9b44b4fb7c3839c1915b0c6e0
itschristianpantilon <pantilonchristian@gmail.com>
2026-03-09 17:20:02 +0800

    refractor api endpoints

--- client/src/api/get-notification-api.ts
+++ client/src/api/get-notification-api.ts
@@ -1,4 +1,5 @@
-import { authFetch, BASE_URL, handleResponse } from "./implementor-api";
+import { authFetch, BASE_URL, handleResponse } from "./api-client";
+
 
 export async function getNotifications(): Promise<any> {
   const res = await authFetch(`${BASE_URL}/admin/notifications/`)
