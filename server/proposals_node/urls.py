2bd426bfb92fb72d4bb49a23bdb61d44fa9c093e
Gwenn01 <arnelgwenn@gmail.com>
2026-03-24 14:26:59 +0800

    reviewer eproposal

--- server/proposals_node/urls.py
+++ server/proposals_node/urls.py
@@ -3,12 +3,15 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from .views import (
     ProposalList,
     AdminProposalList,
-    AdminOverviewView
+    AdminOverviewView,
+    ReviewerApproveProposalView
 )
 
 urlpatterns = [
     # user access proposal
     path("proposals-node/<str:proposal_type>/", ProposalList.as_view(), name="proposal-list"),
+    # reviewer access proposal
+    path("reviewer-approve/<int:proposal_id>/", ReviewerApproveProposalView.as_view(), name="reviewer-approve-proposal"),
     # admin access proposal
     path("admin/proposals-node/<str:proposal_type>/", AdminProposalList.as_view(), name="admin-proposal-list"),
     path("admin/overview-proposals/", AdminOverviewView.as_view(), name="admin-overview"),
